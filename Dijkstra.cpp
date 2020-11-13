02bffa1e6b03b74bf978029b3d9eddeed35091cb
OverPoweredDev <omkar.prabhune19@vit.edu>
2020-11-13 14:40:38 +0530

    added Fuckton of Comments

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -15,28 +15,28 @@ void Dijkstra::djikstra(int startRow, int startCol, int destRow, int destCol, ch
     char envCopy[ROW][COL];
     memcpy(envCopy, env, sizeof(char) * ROW * COL);
 
-    //currPoint is a cursor we use when iterating through the graph
+    //currPoint and nextPoint are cursors we use when iterating through the graph
     Point *currPoint = new point();
     currPoint->row = startRow, currPoint->col = startCol, currPoint->dist = 0, currPoint->prevPoint = nullptr; // initialise the cursor to starting point
 
     Point *nextPoint;
 
+    //this priority queue holds the next unvisited points
     PriorityQueue *priorityQueue = new PriorityQueue();
-
     priorityQueue->push(currPoint); //push starting point in the queue
 
+    // Check all the neighboring points to the current point
+    // - ignore if the point is a wall(W)
+    // - ignore if the point is already visited
+    // - ignore if index is out of range
+    // - update point if distance from starting point is shorter (relaxation)
+    // - backtrack and return if destination point is found
     while (priorityQueue->size() != 0) {
 
-        /*
-         Check all the neighboring points to the current point
-         - ignore if the point is a wall(W)
-         - ignore if the point is already visited
-         - ignore if index is out of range
-         - update point if distance from starting point is shorter (relaxation)
-         - backtrack and return if destination point is found
-        */
-
-
+        // check if point directly ABOVE currPoint is
+        // 1. within maze boundaries    AND
+        // 2. not a Wall                AND
+        // 3. unvisited
         if (currPoint->row - 1 != -1 && env[currPoint->row - 1][currPoint->col] != 'W' && envCopy[currPoint->row - 1][currPoint->col] != '*') {
             nextPoint = new point();
             nextPoint->prevPoint = currPoint;
@@ -44,15 +44,23 @@ void Dijkstra::djikstra(int startRow, int startCol, int destRow, int destCol, ch
             nextPoint->col = currPoint->col;
             nextPoint->dist = currPoint->dist + 1;
 
+            // check if nextPoint is the Destination
             if (nextPoint->row == destRow && nextPoint->col == destCol) {
                 backTrack(nextPoint, env);
                 return;
             }
+
+            // if not destination, mark it as visited and push it to the queue IF its distance from Start is lesser
             envCopy[nextPoint->row][nextPoint->col] = '*';
             if (!priorityQueue->relaxation(nextPoint)) {
                 priorityQueue->push(nextPoint);
             }
         }
+
+        // check if point to the LEFT of currPoint is
+        // 1. within maze boundaries    AND
+        // 2. not a Wall                AND
+        // 3. unvisited
         if (currPoint->col - 1 != -1 && env[currPoint->row][currPoint->col - 1] != 'W' && envCopy[currPoint->row][currPoint->col - 1] != '*') {
             nextPoint = new point();
             nextPoint->prevPoint = currPoint;
@@ -60,16 +68,23 @@ void Dijkstra::djikstra(int startRow, int startCol, int destRow, int destCol, ch
             nextPoint->col = currPoint->col - 1;
             nextPoint->dist = currPoint->dist + 1;
 
+            // check if nextPoint is the Destination
             if (nextPoint->row == destRow && nextPoint->col == destCol) {
                 backTrack(nextPoint, env);
                 return;
             }
 
+            // if not destination, mark it as visited and push it to the queue IF its distance from Start is lesser
             envCopy[nextPoint->row][nextPoint->col] = '*';
             if (!priorityQueue->relaxation(nextPoint)) {
                 priorityQueue->push(nextPoint);
             }
         }
+
+        // check if point to the RIGHT of currPoint is
+        // 1. within maze boundaries    AND
+        // 2. not a Wall                AND
+        // 3. unvisited
         if (currPoint->col + 1 != COL && env[currPoint->row][currPoint->col + 1] != 'W' && envCopy[currPoint->row][currPoint->col + 1] != '*') {
             nextPoint = new point();
             nextPoint->prevPoint = currPoint;
@@ -77,16 +92,23 @@ void Dijkstra::djikstra(int startRow, int startCol, int destRow, int destCol, ch
             nextPoint->col = currPoint->col + 1;
             nextPoint->dist = currPoint->dist + 1;
 
+            // check if nextPoint is the Destination
             if (nextPoint->row == destRow && nextPoint->col == destCol) {
                 backTrack(nextPoint, env);
                 return;
             }
 
+            // if not destination, mark it as visited and push it to the queue IF its distance from Start is lesser
             envCopy[nextPoint->row][nextPoint->col] = '*';
             if (!priorityQueue->relaxation(nextPoint)) {
                 priorityQueue->push(nextPoint);
             }
         }
+
+        // check if point directly BELOW currPoint is
+        // 1. within maze boundaries    AND
+        // 2. not a Wall                AND
+        // 3. unvisited
         if (currPoint->row + 1 != ROW && env[currPoint->row + 1][currPoint->col] != 'W' && envCopy[currPoint->row + 1][currPoint->col] != '*') {
             nextPoint = new point();
             nextPoint->prevPoint = currPoint;
@@ -94,11 +116,13 @@ void Dijkstra::djikstra(int startRow, int startCol, int destRow, int destCol, ch
             nextPoint->col = currPoint->col;
             nextPoint->dist = currPoint->dist + 1;
 
+            // check if nextPoint is the Destination
             if (nextPoint->row == destRow && nextPoint->col == destCol) {
                 backTrack(nextPoint, env);
                 return;
             }
 
+            // if not destination, mark it as visited and push it to the queue IF its distance from Start is lesser
             envCopy[nextPoint->row][nextPoint->col] = '*';
             if (!priorityQueue->relaxation(nextPoint)) {
                 priorityQueue->push(nextPoint);
