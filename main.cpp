f19b2a8110c9c007ca5b9141f5015b9cd82cb939
prabhav-pandya <business.prabhav@gmail.com>
2021-12-15 23:00:48 +0530

    implemented dijkstra

--- main.cpp
+++ main.cpp
@@ -19,11 +19,18 @@ struct Position{
     int cost;
 };
 
+int row, col;
+
 vector<vector<char>> env;
+
+vector<vector<char>> envClone;
+
+int numIterations = 0;
 
 //char playerChar = 'P';
 //char wallChar = 'B';
 //char goalChar = 'G';
+
 
 // Compare Heuristic
 class CompareHeuristic {
@@ -65,8 +72,117 @@ void printSolvedEnv(Position lastPos, Position startPos){
         }
         cout<<endl;
     }
-    cout<<"\nPath Length: "<<len;
+    cout<<"\nPath Length: "<<len<<endl;
+    cout<<"Number of iterations: "<<numIterations<<endl;
 }
+
+// can we update the window as the algorithm explores using multi threading?
+int drawEnv(vector<vector<char>> env, int row, int col) {
+   int squareSize = 30;
+    sf::RenderWindow window(sf::VideoMode(col*squareSize, row*squareSize), "Pathfinder");
+    vector<sf::RectangleShape> vec;
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            int x = i * squareSize;
+            int y = j * squareSize;
+
+            sf::RectangleShape whitecolor(sf::Vector2f(squareSize, squareSize)); //White rectangles for the board
+
+            if (env[i][j] == wallChar) whitecolor.setFillColor(sf::Color(36, 8, 150));
+
+            else if (env[i][j] == playerChar|| env[i][j] == goalChar) whitecolor.setFillColor(sf::Color(117, 40, 166));
+
+            else if (env[i][j] == 'o') whitecolor.setFillColor(sf::Color(176, 66, 255));
+
+            else if (env[i][j] == '_')  whitecolor.setFillColor(sf::Color(180, 161, 255));
+
+            whitecolor.setPosition(sf::Vector2f(y, x));
+
+            vec.push_back(whitecolor);
+        }
+    }
+  while (window.isOpen()) {
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+                window.close();
+            }
+        }
+
+        window.clear();
+        for (auto &rect : vec) {
+            window.draw(rect);
+        }
+        window.display();
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+
+
+void dijkstra(vector<vector<char>> board) {
+    int numRows = board.size(), numCols = board[0].size();
+
+    Position playerPos = findPos(board, playerChar);
+    Position goalPos = findPos(board, goalChar);
+
+    playerPos.heuristicVal = 0;
+
+    // maintain a open and closed list
+    vector<Position> closedList;
+    priority_queue<Position, vector<Position>, CompareHeuristic> openList;
+    openList.push(playerPos);
+
+    Position currentPos{};
+
+    int adjMovesRow[] = {0,1,0,-1};
+    int adjMovesCol[] = {1,0,-1,0};
+
+    while(calculateManhattan(currentPos, goalPos)!=0 && !openList.empty()){
+
+        currentPos = openList.top();
+
+        envClone[currentPos.row][currentPos.col] = 'o';
+
+        numIterations++;
+
+        openList.pop();
+        for(int i=0;i<4;i++){
+            if((currentPos.row + adjMovesRow[i]<numRows && currentPos.row + adjMovesRow[i]>=0) && (currentPos.col + adjMovesCol[i]<numCols && currentPos.col + adjMovesCol[i]>=0)){
+                if(board[currentPos.row+adjMovesRow[i]][currentPos.col+adjMovesCol[i]]!=wallChar){
+                    Position newMove = Position{currentPos.row+adjMovesRow[i], currentPos.col+adjMovesCol[i]};
+
+                    // check if position already exists in closed List
+                    bool isInClosedList = false;
+                    for(int k=0;k<closedList.size();k++){
+                        if(closedList[k].row==currentPos.row && closedList[k].col==currentPos.col) isInClosedList = true;
+                    }
+                    newMove.heuristicVal = currentPos.heuristicVal+1;
+                    Position lastPos;
+                    lastPos.row = currentPos.row;
+                    lastPos.col = currentPos.col;
+                    lastPos.lastPos = currentPos.lastPos;
+                    newMove.lastPos = new Position{currentPos.row, currentPos.col, currentPos.heuristicVal, currentPos.lastPos};
+                    if(!isInClosedList) {
+                        openList.push(newMove);
+                    }
+                }
+            }
+        }
+        closedList.push_back(currentPos);
+    }
+
+    if(calculateManhattan(currentPos,goalPos)==0) printSolvedEnv(currentPos, playerPos);
+    else cout<<"\nNo path exists.";
+}
+
+
 
 
 void bfs(vector<vector<char>> board){
@@ -91,6 +207,10 @@ void bfs(vector<vector<char>> board){
     while(calculateManhattan(currentPos, goalPos)!=0 && !openList.empty()){
 
         currentPos = openList.top();
+
+        envClone[currentPos.row][currentPos.col] = 'o';
+
+        numIterations++;
 
         openList.pop();
         for(int i=0;i<4;i++){
@@ -147,6 +267,7 @@ void astar(vector<vector<char>> board){
 
         currentPos = openList.top();
 
+        numIterations++;
         openList.pop();
         for(int i=0;i<4;i++){
             if((currentPos.row + adjMovesRow[i]<numRows && currentPos.row + adjMovesRow[i]>=0) && (currentPos.col + adjMovesCol[i]<numCols && currentPos.col + adjMovesCol[i]>=0)){
@@ -207,59 +328,10 @@ void printEnv(){
 }
 
 
-int drawEnv(vector<vector<char>> env, int row, int col) {
-    int squareSize = 30;
-    sf::RenderWindow window(sf::VideoMode(col*squareSize, row*squareSize), "Pathfinder");
-    vector<sf::RectangleShape> vec;
-
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            int x = i * squareSize;
-            int y = j * squareSize;
-
-            sf::RectangleShape whitecolor(sf::Vector2f(squareSize, squareSize)); //White rectangles for the board
-
-            if (env[i][j] == wallChar) whitecolor.setFillColor(sf::Color(36, 8, 150));
-
-            else if (env[i][j] == playerChar|| env[i][j] == goalChar) whitecolor.setFillColor(sf::Color(117, 40, 166));
-
-            else if (env[i][j] == 'o') whitecolor.setFillColor(sf::Color(176, 66, 255));
-
-            else if (env[i][j] == '_')  whitecolor.setFillColor(sf::Color(180, 161, 255));
-
-            whitecolor.setPosition(sf::Vector2f(y, x));
-
-            vec.push_back(whitecolor);
-        }
-    }
-
-    while (window.isOpen()) {
-        sf::Event event{};
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
-                window.close();
-            }
-        }
-
-        window.clear();
-        for (auto &rect : vec) {
-            window.draw(rect);
-        }
-
-        window.display();
-    }
-
-    return EXIT_SUCCESS;
-}
-
 
 
 int main()
 {
-    int row, col;
     char ch;
     cout<<"Would you like to automatically generate a maze or design one manually? (y/n) "<<endl;
     cin>>ch;
@@ -287,19 +359,22 @@ int main()
         row = ROW;
         col = COL;
     }
-    int choice;
-    cout<<"\nChoose:\n1. BFS\n2. A*\n";
-    cin>>choice;
 
+    envClone = env;
+
+    int choice;
+    cout<<"\nChoose:\n1. BFS\n2. A*\n3. Dijkstra\n";
+    cin>>choice;
     switch(choice){
         case 1: bfs(env);
             break;
         case 2: astar(env);
             break;
+        case 3: dijkstra(env);
+            break;
         default: cout<<"\nWrong input!";
             break;
     }
-
     drawEnv(env, row, col);
 
     return 0;
